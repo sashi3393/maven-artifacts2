5ffaad71151280da3c60aed451ac3b24322d6547
sashi <shanmugasrinivas48@gmail.com>
2021-02-25 09:31:18 +0530

    commit 1

---
+++ src/test/java/project2/maintest.java
@@ -0,0 +1,14 @@
+package project2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class maintest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
